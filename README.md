fbaee12d847063bdf062f385ccacbaa7c4612840
Jonas Skovmand <jonas@satf.se>
2012-01-18 10:22:15 +0100

    SimpleNote integration, etc.
    * SimpleNote integration. Save, open and update your SimpleNotes.
    * Added a modal box for SimpleNote interaction (needs styling)
    * Moved JS loading to ``<head>``, since it needs to be loaded before
    any interaction is made.
    * Moved js libs into one single file.
    * Bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Owwmriter
-Initial commit of Owwmriter, a HTML5 web clone of the wonderful Ommwriter. Features a seamless interface where writing is top priority and enhanced with background sound, keyboard clicks, a soothing background image and many other features.
+``OWWMRITER``, a HTML5 web clone of the wonderful [Ommwriter](http://www.ommwriter.com/). Features a seamless interface where writing is top priority and enhanced with background sound, keyboard clicks, a soothing background image and many other features.
+
+The goal of this clone is, for a start, to be a complete feature by feature clone of Ommwriter, and to work without any backend utilizing localStorage and [SimpleNote-js](https://github.com/carlo/simplenote-js) api.
+
+**WARNING** This is still very much an ALPHA. Features are experimental at best. Check back in a couple of months and perhaps it's more stable...
 
 ## Features & future
 
@@ -10,20 +14,37 @@ Initial commit of Owwmriter, a HTML5 web clone of the wonderful Ommwriter. Featu
 * Keyboard sound
 * Different typefaces & font sizes
 * Saves your settings
+* SimpleNote integration
 
 ### On the todo list, and feature wish list:
-* Saving capability
 * Auto save, iterative save
-* Save to cloud service like Dropbox
+* Other third party storage integration (like Dropbox)
 * Fix scaling of the textarea. (Currently weird in Webkit-based browsers)
 * Add a splash screen for loading
 * Export to txt, pdf and various formats
+* iPad/Tablet compatibility (sounds)
 * Bug fixes!!!
 
 ### If I were a better programmer
-* Clone the dynamic ambient backgrounds of Ommwriter
+* Clone the dynamic ambient backgrounds of Ommwriter in webgl
+
+## Changelog
+* Added favicon to header
+* SimpleNote integration. Save, open and update your SimpleNotes.
+* Added a modal box for SimpleNote interaction (needs styling)
+* Moved JS loading to ``<head>``, since it needs to be loaded before any interaction is made.
+* Moved js libs into one single file.
+* Bug fixes
 
 ## Notes
-OWWMRITER or its creator, Jonas Skovmand, is in no way endorsed or affiliated with Ommwriter or its creators & copyright holders, Herraiz Soto & Co. OWWMRITER is just a fun remake of the amazing application Ommwriter, to test what can be accomplished with modern web technologies.
+
+### Experimental & unsafe operations
+The SimpleNote integration is currently tunnelled over HTTP and Yahoo! Pipes, which basically means that your email, password and notes are sent unencrypted and is visible to anyone with a little packet-fu. Use with care, and don't use ``OWWMRITER`` if you have any sensitive information like government secrets or your next sure-fire best selling book.
+
+### Copyright
+``OWWMRITER`` or its creator, Jonas Skovmand, is in no way endorsed or affiliated with Ommwriter or its creators & copyright holders, Herraiz Soto & Co. ``OWWMRITER`` is just a fun remake of the amazing application Ommwriter, to test what can be accomplished with modern web technologies.
 
 If you like it, buy the real Ommwriter OS X or iPad app from http://www.ommwriter.com. You'll love it even more than this!
+
+### License
+``OWWMRITER``'s codebase is released under the MIT License. See the ``LICENSE`` file for more details.
--- js/script.js
+++ js/script.js
@@ -1,8 +1,157 @@
 /* Author: Jonas Skovmand <jonas@satf.se>
 This is in no way endorsed by or affiliated with Herraiz Soto & Co or OMMWRITER.
-
 */
 
+var OWWMRITER_SimpleNote = (function () {
+	var O_sn = this, O_auth = false, O_token = false, instance;
+	
+	function isAuthed () {
+		return O_auth === true;
+	}
+	
+	function getInstance () {
+		if ( typeof instance === 'undefined' )
+		{
+			instance = new SimpleNote();
+		}
+		
+		return instance;
+	}
+	
+	function handleAuth(auth)
+	{
+		if ( auth.email && auth.token )
+		{
+			O_auth = true;
+		}
+		else
+		{
+			O_auth = false;
+		}
+	}
+	
+	function fakeAuth ( obj ) {
+		var SN = this.getInstance();
+		SN.fakeAuth(obj);
+	}
+	
+	function auth ( email, password, callback ) {
+		var SN = this.getInstance();
+		SN.auth({
+			"email": email,
+			"password": password,
+			success: function() {
+				OWWMRITER_SimpleNote.handleAuth(SN.getAuthDetails());
+				if ( typeof callback === 'function' )
+				{
+					callback({response:'success', auth: SN.getAuthDetails()});
+				}
+			},
+			error: function( code ) {
+				if ( typeof callback === 'function' )
+				{
+					callback({response:'error', message: code});
+				}
+			}
+		});
+	}
+	
+	function list ( callback ) {
+		var SN = this.getInstance();
+		SN.retrieveIndex({
+			success: function( resultsArray ) {
+				var l = resultsArray.length;
+				for ( var i=0; i<l; i++)
+				{
+					if ( resultsArray[i].deleted == true ) continue; // ignore deleted
+					SN.retrieveNote({
+						key: resultsArray[i].key,
+						success: function(notehash){
+							if ( typeof callback === 'function' )
+							{
+								callback( { response: 'success', result: notehash });
+							}
+						},
+						error: function (code) {
+							callback( { response: 'error', message: code });
+						}
+					});
+				}
+				
+			},
+			error: function( code ) {
+				if ( typeof callback === 'function' )
+				{
+					callback( { response: 'error', message: code } );
+				}
+			}
+		});
+	}
+
+	function open ( obj ) {
+		var SN = this.getInstance();
+		SN.retrieveNote({
+			"key": obj.key,
+			success: obj.success,
+			error: obj.error
+		});
+	}
+	
+	function create ( obj ) {
+		
+		if ( !obj.success || !obj.error ) {
+			throw "Callback error";
+		}
+		
+		var SN = this.getInstance();
+		SN.createNote({
+			"body": obj.body,
+			success: obj.success,
+			error: obj.error
+		});
+	}
+	
+	function save ( key, body ) {
+		var SN = this.getInstance();
+		
+		if ( !key )
+		{
+			SN.createNote({
+				"body": body,
+				success: function( noteID ) {
+					console.info( noteID );
+				},
+				error: function( code ) {
+					console.error( code );
+				}
+			});
+		}
+
+		SN.updateNote({
+			"key": key,
+			"body": body,
+			success: function( noteID ) {
+				console.info( noteID );
+				// >> "[SimpleNote-internal ID string]"
+			},
+			error: function( code ) {
+				console.error( code );
+			}
+		});
+	}
+	
+	return {
+		isAuthed: isAuthed,
+		getInstance: getInstance,
+		fakeAuth: fakeAuth,
+		auth: auth,
+		list: list,
+		open: open,
+		save: save,
+		create: create,
+		handleAuth: handleAuth
+	};
+})();
 
 var OWWMRITER = (function() {
 	var oww = this;
@@ -15,7 +164,9 @@ var OWWMRITER = (function() {
 		audioMode: 'wav'
 	};
 	
-	this.submergeTimer;
+	OWWMRITER_SimpleNote.getInstance().setOpenDataTable('http://xn--stf-qla.se/owwmriter/js/yql_simplenote.xml');
+	
+	this.snnotes = [];
 	this.submerged = false;
 	this._keyboardSounds = [];
 	
@@ -32,15 +183,20 @@ var OWWMRITER = (function() {
 	this.user_settings = this.sticky.get(this.settings.saveKey) || {};
 	
 	this.moved = function() {
-		if ( this.submerged )
+		if ( this.submerged && !$('#colorbox').is(':visible') )
+		{
 			this.resurface();
+		}
 		clearTimeout(this.submergeTimer);
 		this.submergeTimer = setTimeout(this.submerge, this.settings.submergeTimeout);
 	};
 	
 	this.submerge = function () {
 		this.submerged = true;
-		$('#canvas').focus();
+		if ( !$('#colorbox').is(':visible') )
+		{
+			$('#canvas').focus();
+		}
 		$('body').addClass('oww-submerged');
 	};
 	
@@ -55,7 +211,7 @@ var OWWMRITER = (function() {
 	};
 	
 	this.setKeyboardSound = function ( value ) {
-		if ( value == 'off' )
+		if ( value === 'off' )
 		{
 			// disable keyboard sound.
 			$('#canvas').unbind('keyup.keyboardsounds');
@@ -69,7 +225,7 @@ var OWWMRITER = (function() {
 		var sounds = value[1].split(',');
 		for ( var i=0; i<sounds.length; i++ )
 		{
-			this._keyboardSounds[i] = new Audio;
+			this._keyboardSounds[i] = new Audio();
 			this._keyboardSounds[i].src = this.settings.keyboardSoundFolder + sounds[i] + '.' + this.settings.audioMode;
 			this._keyboardSounds[i].volume = parseFloat(volume);
 			this._keyboardSounds[i].load();
@@ -82,7 +238,7 @@ var OWWMRITER = (function() {
 	};
 	
 	this.setBackgroundSound = function ( value ) {
-		if ( value == 'off' )
+		if ( value === 'off' )
 		{
 			// disable keyboard sound.
 			if ( this._backgroundSound )
@@ -100,15 +256,14 @@ var OWWMRITER = (function() {
 		
 		if ( !this._backgroundSound )
 		{
-			this._backgroundSound = new Audio;
-			this._backgroundSound.addEventListener('ended', function(){this.play()}, false);
+			this._backgroundSound = new Audio();
+			this._backgroundSound.addEventListener('ended', function(){this.play();}, false);
 		}
 		else
 		{
 			this._backgroundSound.pause();
 			this._backgroundSound.currentTime = 0;
 		}
-		
 		
 		sound = this.settings.backgroundSoundFolder + sound + '.' + this.settings.audioMode;
 		this._backgroundSound.src = sound;
@@ -121,7 +276,7 @@ var OWWMRITER = (function() {
 		var $canvas = $('#canvas');
 		if ( $canvas.data('oww-font-size') )
 		{
-			$canvas.removeClass($canvas.data('oww-font-size'))
+			$canvas.removeClass($canvas.data('oww-font-size'));
 		}
 		$canvas.data('oww-font-size',value).addClass(value);
 	};
@@ -131,40 +286,214 @@ var OWWMRITER = (function() {
 		var $canvas = $('#canvas');
 		if ( $canvas.data('oww-font') )
 		{
-			$canvas.removeClass($canvas.data('oww-font'))
+			$canvas.removeClass($canvas.data('oww-font'));
 		}
 		$canvas.data('oww-font',value).addClass(value);
 	};
 	
-	this.utilityAction = function ( value ) {
-		// save etc.
+	this.countWords = function ( string ) {
+		return string.length === 0 ? 0 : string.match(/\b\S+\b/g).length;
 	};
 	
-	this.countWords = function ( string ) {
-		return string.length == 0 ? 0 : string.match(/\b\S+\b/g).length;
+	this.loginPrompt = function ( ){
+		this.submerge();
+		$.colorbox({
+			href:'#simplenote-login',
+			inline:true,
+			onComplete:function(){$('#colorbox input:first').focus();}
+		});
+		return false;
+	};
+	
+	this.checkAuth = function ( obj ) {
+		var auth = OWWMRITER_SimpleNote.isAuthed(), savedAuth = this.getSavedAuth();
+		
+		if ( !auth && typeof savedAuth !== 'undefined' && savedAuth.email && savedAuth.token )
+		{
+			OWWMRITER_SimpleNote.fakeAuth($.extend({}, savedAuth, {
+				success: function(){
+					OWWMRITER_SimpleNote.handleAuth(savedAuth);
+					obj.success();
+				},
+				error: function(){
+					OWWMRITER_SimpleNote.handleAuth({});
+					oww.removeSavedAuth();
+					obj.error();
+				}
+			}));
+			return false;
+		}
+		
+		if ( !auth )
+		{
+			this.loginPrompt();
+		}
+		
+		return true;
+	};
+	
+	this.save = function ( saveas ) {
+		var key = this.getKey(), saveas = typeof saveas == 'undefined' || !saveas || saveas == 'save' ? false : true, res = this.checkAuth({
+			success: function(){
+				console.log('saveas', saveas);
+				oww.save(saveas);
+			},
+			error: function(){
+				oww.save(saveas);
+			}
+		});
+		
+		if ( !res ) return;
+		
+		if ( typeof key == 'undefined' || saveas )
+		{
+			this.submerge();
+			this.saveAsDialog();
+			return;
+		}
+		
+		OWWMRITER_SimpleNote.save(key, $('#canvas').val());
+	};
+	
+	this.saveas = function () {
+		this.save(true);
+	};
+	
+	this.open = function () {
+		var res = this.checkAuth({
+			success: function(){
+				oww.open();
+			},
+			error: function(){
+				oww.open();
+			}
+		});
+			
+		if ( !res ) return;
+		this.submerge();
+		this.snnotes = [];
+		OWWMRITER_SimpleNote.list(this.populateOpenCallback);
+	};
+	
+	this.load = function ( key ) {
+		this.saveKey(key);
+		OWWMRITER_SimpleNote.open({
+			'key': key,
+			success: function(res) { $('#canvas').val(res.body); },
+			error: function(res) { console.log('ERROR',res); }
+		});
 	}
 	
-	this.save = function ( key, value ) {
-		if ( key == 'utilities' ) return;
+	this.saveSettings = function ( key, value ) {
+		if ( key === 'utilities' )
+		{
+			return;
+		}
 		this.user_settings[key] = value;
 		this.sticky.set(this.settings.saveKey, this.user_settings);
+	};
+	
+	this.getSetting = function ( key ) {
+		return this.user_settings[key];
+	};
+	
+	this.removeSetting = function ( key ) {
+		delete this.user_settings[key];
+		this.sticky.set(this.settings.saveKey, this.user_settings);
+	};
+	
+	this.getSavedAuth = function ( ) {
+		return this.getSetting('sn');
+	};
+	
+	this.removeSavedAuth = function ( ) {
+		this.removeSetting('sn');
+	};
+	
+	this.saveAuth = function ( ) {
+		this.saveSettings('sn', OWWMRITER_SimpleNote.getInstance().getAuthDetails());
+	};
+	
+	this.getKey = function ( ) {
+		return this.getSetting('sncurrentkey');
+	};
+	
+	this.saveKey = function ( key ) {
+		this.saveSettings ( 'sncurrentkey', key );
+	};
+	
+	this.saveAsDialog = function ( ) {
+		this.snnotes = [];
+		OWWMRITER_SimpleNote.list(this.populateSaveAsCallback);
+	};
+	
+	this.newSimpleNote = function ( ) {
+		OWWMRITER_SimpleNote.create({
+			"body": $('#canvas').val(),
+			success: function ( key ) {
+				oww.saveKey ( key );
+			},
+			error: function ( code ) {
+				console.log ('ERROR', code );
+			}
+		});
+	};
+	
+	this.populateCallback = function ( response, $el ) {
+		if ( response.response == 'success' )
+		{
+			if ( this.snnotes.length === 0 )
+			{
+				$el.find('ul li').remove();
+				$.colorbox({
+					href:$el.selector,
+					width: '500px',
+					inline:true
+				});
+			}
+			
+			if ( response.result.deleted )
+			{
+				return;
+			}
+			
+			this.snnotes.push(response.result);
+			$el.find('ul').append($('<li/>').data('key', response.result.key).text(response.result.body.split("\n")[0]));
+			
+		}
+		else
+		{
+			// ERROR
+			console.log('ERROR', response.message);
+			console.log(response);
+		}
 	}
+	
+	this.populateOpenCallback = function ( response ) {
+		this.populateCallback ( response, $('#simplenote-list-open') );
+	};
+	
+	this.populateSaveAsCallback = function ( response ) {
+		this.populateCallback ( response, $('#simplenote-list') );
+	};
 	
 	return this;
 })();
+
 
 $(function(){
 	
 	$('#toolbar .oww-menu li')
 		.hover(function(){
 			var $o = $(this);
-			if ( $o.hasClass('oww-enabled') ) return;
+			if ( $o.hasClass('oww-enabled') ) {
+				return;
+			}
 			$o.addClass('oww-hover');
 		}, function(){
 			$(this).removeClass('oww-hover');
 		}).click(function(){
-			$o = $(this);
-			var $old = $o.siblings('.oww-enabled')
+			var $o = $(this), $old = $o.siblings('.oww-enabled');
 			if ( $old.length > 0 )
 			{
 				var $oimg = $old.first().removeClass('oww-enabled').find('img');
@@ -174,17 +503,16 @@ $(function(){
 			$o.removeClass('oww-hover').addClass('oww-enabled');
 			var $img = $o.find('img');
 			$img.attr('src', $img.attr('src').replace('_', '') );
-			OWWMRITER.save($o.parents('.oww-menu').attr('data-menu'), $o.attr('data-key'));
+			OWWMRITER.saveSettings($o.parents('.oww-menu').attr('data-menu'), $o.attr('data-key'));
 			OWWMRITER[$o.attr('data-function')]($o.attr('data-value'));
 			$('#canvas').focus();
 			return false;
 		})
 	;
 	
-	
 	var _saved = OWWMRITER.user_settings;
 	
-	if ( _saved != null )
+	if ( _saved !== null )
 	{
 		$('#toolbar .oww-menu').each(function(index){
 			var key = $(this).attr('data-menu');
@@ -215,5 +543,46 @@ $(function(){
 	$('#canvas').bind('keyup.canvas click.canvas blur.canvas focus.canvas change.canvas paste.canvas', function(event){
 		$('#wordcount').text(OWWMRITER.countWords($(this).val()));
 	}).focus();
+	
+	$('#simplenote-create-new').live('click', function(){
+		OWWMRITER.newSimpleNote();
+		$.colorbox.close();
+		return false;
+	});
+	
+	$('#simplenote-list-refresh').live('click', function(){
+		OWWMRITER.snnotes = [];
+		OWWMRITER.saveAsDialog();
+		return false;
+	});
+	
+	$('#simplenote-login-form').live('submit', function(){
+		var O_form = $(this);
+		OWWMRITER_SimpleNote.auth(O_form.find('input[name=email]').val(), O_form.find('input[name=password]').val(), function(data){
+			if ( data.response === 'success' )
+			{
+				OWWMRITER.saveAuth();
+				OWWMRITER.saveAsDialog();
+			}
+			else
+			{
+				console.log('ERROR', data.message);
+				console.log(data);
+			}
+		});
+		return false;
+	});
+	
+	$('#simplenote-list-notes li').live('click', function(){
+		OWWMRITER.saveKey($(this).data('key'));
+		$.colorbox.close();
+		return false;
+	});
+	
+	$('#simplenote-list-open-notes li').live('click', function(){
+		OWWMRITER.load($(this).data('key'));
+		$.colorbox.close();
+		return false;
+	});
 	
 });
